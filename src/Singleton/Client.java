dbaaef74df4996f80e07ae371be76db9472e7e52
Madhavi <madhaviparepalli8@gmail.com>
2025-05-21 15:27:58 +0530

    Implemented threadsafe Singleton

--- src/Singleton/Client.java
+++ src/Singleton/Client.java
@@ -2,12 +2,35 @@ package Singleton;
 
 public class Client {
     public static void main(String[] args) {
+        /*for 1st basic singleton ex these are to debug
         DBConnection dbc1 =DBConnection.getInstance();
         DBConnection dbc2 =DBConnection.getInstance();
         DBConnection dbc3 =DBConnection.getInstance();
+        Thread[] threads = new Thread[10];
         System.out.println(dbc1);
         System.out.println(dbc2);
         System.out.println(dbc3);
+
+         */
+        Runnable task = () -> {
+            DBConnection dbc = DBConnection.getInstance();
+            System.out.println(Thread.currentThread().getName() + " got instance " + dbc.hashCode());
+        };
+        Thread[] threads = new Thread[10];
+        for (int i = 0; i < threads.length; i++) {
+            threads[i] = new Thread(task , "Thread " + i);
+            threads[i].start();
+        }
+        // Wait for all threads to complete before exiting main
+        for(int i = 0; i < threads.length; i++) {
+            try{
+                threads[i].join();
+            }
+            catch (InterruptedException e){
+                e.printStackTrace();
+            }
+        }
+
 
     }
 }
--- src/Singleton/DBConnection.java
+++ src/Singleton/DBConnection.java
@@ -1,7 +1,11 @@
 package Singleton;
 
+import java.util.concurrent.locks.ReentrantLock;
+
 public class DBConnection {
     private static DBConnection instance = null;
+    private static final Object lock = new Object();
+    private static final ReentrantLock reentrantLock = new ReentrantLock();
 
     String url;
     String username;
@@ -10,9 +14,25 @@ public class DBConnection {
     private DBConnection() {
 
     }
+    //1 way using object and sync block
+   /* public static DBConnection getInstance() {
+        if (instance == null) {
+            synchronized (lock) {
+                if (instance == null) {
+                    instance = new DBConnection();
+                }
+            }
+        }
+        return instance;
+    }*/
+    //2nd way using Reentrant lock
     public static DBConnection getInstance() {
         if (instance == null) {
-            instance = new DBConnection();
+            reentrantLock.lock();
+            if (instance == null) {
+                instance = new DBConnection();
+            }
+            reentrantLock.unlock();
         }
         return instance;
     }
